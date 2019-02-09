304d0d85afb982e87fe26c6e7178e3e3cafe807b
Sebastian Meßmer <smessmer@users.noreply.github.com>
2019-02-08 23:57:28 -0800

    Update cryfs.rb

--- Formula/cryfs.rb
+++ Formula/cryfs.rb
@@ -17,9 +17,9 @@ class Cryfs < Formula
 
   depends_on "cmake" => :build
   depends_on "boost"
+  depends_on "libomp"
   depends_on "openssl"
   depends_on :osxfuse
-  depends_on "libomp"
 
   def install
     configure_args = [
